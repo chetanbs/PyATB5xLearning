1438d9ccccb6aada0fb54c6456fe7a852edab302
Chetan B Sangali <chetanbsangali@gmail.com>
2024-11-17 10:22:23 +0530

    12th Nov Tasks Programs

--- Nov/ex_12112024_Operators/Lab045_Task2.py
+++ Nov/ex_12112024_Operators/Lab045_Task2.py
@@ -1,6 +1,6 @@
 #Display the output
 
-# As we have not declared/defines a in the program, will get error in output as "NameError: name 'a' is not defined"
+# As we have not declared/defined a in the program, will get error in output as "NameError: name 'a' is not defined"
 print(type(a))
 if a == 10:
     print("Hello World")
