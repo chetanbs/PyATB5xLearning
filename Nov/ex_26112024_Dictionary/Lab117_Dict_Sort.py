71d661f5f4197b9e46d03f5f8ca04d1cb3b1142a
Chetan B Sangali <chetanbsangali@gmail.com>
2024-11-30 08:37:52 +0530

    sort

--- Nov/ex_26112024_Dictionary/Lab117_Dict_Sort.py
+++ Nov/ex_26112024_Dictionary/Lab117_Dict_Sort.py
@@ -4,8 +4,15 @@ dict2 = {"a": 1, "b": 2}
 missing_keys = set(dict1.keys() - set(dict2.keys()))
 print(missing_keys)
 
+
+
+
 # Sort a dictionary by its values in descending order
 my_dict = {"a": 3, "b": 1, "c": 2}
 
 #O/P -> {b:1, c:2, a:3}
+
+sort_by_values = dict(sorted(my_dict.items(), key=lambda item:item[1]))
+print(sort_by_values)
+
 
